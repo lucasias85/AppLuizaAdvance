841993d2220868a41429a1b5aa00bea1994d1dca
Carlos Assunção <lucasias85@gmail.com>
2022-07-11 18:18:25 -0300

    Add project files.

---
+++ AppLuizaAdvance/AppLuizaAdvance/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using AppLuizaAdvance.Views;
+using Xamarin.Forms;
+
+namespace AppLuizaAdvance
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
