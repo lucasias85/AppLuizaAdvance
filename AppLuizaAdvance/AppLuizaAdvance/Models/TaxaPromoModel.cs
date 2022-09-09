43740536c461af62021fb16a123e62c34ba23e58
Luiz Silva <Luiz Silva@LUIZ-SILVA>
2022-09-09 14:28:21 -0300

    Add project files.

---
+++ AppLuizaAdvance/AppLuizaAdvance/Models/TaxaPromoModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLuizaAdvance.Models
+{
+    class TaxaPromoModel
+    {
+    }
+}
---
+++ AppLuizaAdvance/AppLuizaAdvance/ViewModels/TaxaPromoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLuizaAdvance.ViewModels
+{
+    class TaxaPromoViewModel
+    {
+    }
+}
