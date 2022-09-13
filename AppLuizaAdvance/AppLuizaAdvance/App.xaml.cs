4dcb7a5575e9a96f12703546c518e3837e8b011b
Luiz Silva <luiz.silva@cncred.com.br>
2022-09-13 17:38:01 -0300

    #NovoTaxaPromoPage Front-End

--- AppLuizaAdvance/AppLuizaAdvance/App.xaml.cs
+++ AppLuizaAdvance/AppLuizaAdvance/App.xaml.cs
@@ -11,7 +11,7 @@ namespace AppLuizaAdvance
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new NovoTaxaPromoPage());
         }
 
         protected override void OnStart()
